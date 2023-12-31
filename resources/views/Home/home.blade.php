00afb17b657a783c743b09c34fba1eb24970b2a4
nurulabsr <absar.ru@yandex.com>
2023-12-31 16:44:08 +0600

    Updated Authentication page, added logout, login, register in the navbar

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -22,26 +22,34 @@
                     <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
                 </li>
                 @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
+                        <li class="nav-item">
+                            <a href="{{ url('/dashboard') }}" class="nav-link active text-light">Dashboard</a>
+                        </li>
+                        {{-- <x-slot name="content"> --}}
+                        <li class="nav-item">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                         this.closest('form').submit();">
+                                    
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </li>
+                        {{-- </x-slot> --}}
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                        <li  class="nav-item">
+                            <a href="{{ route('login') }}" class="nav-link active text-light">Log in</a>
+                        </li>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                            <li class="nav-item">
+                                <a href="{{ route('register') }}" class="nav-link active text-light">Register</a>
+                            </li>
                         @endif
                     @endauth
-                </div>
             @endif
             </ul>
             <span class="navbar-text text-light">
