2304ed7af25f32433d1c97183a428aabe42c791e
nurulabsr <absar.ru@yandex.com>
2023-12-31 22:35:39 +0600

    added attribute in the users table and added new table i.e, user-roles to categories user role

---
+++ database/migrations/2023_12_31_161431_add_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('user_profile_picture');
+            $table->string('user_role_id')->default(0);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+           $table->dropSoftDeletes();
+        });
+    }
+};
