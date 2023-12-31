81c6b3d6a61e2f547caaf9edd7d0c4e696dbeb1f
nurulabsr <absar.ru@yandex.com>
2023-12-31 11:28:15 +0600

    Authentication setup

--- resources/views/Product/AirbusHomeData.blade.php
+++ resources/views/Product/AirbusHomeData.blade.php
@@ -1,11 +1,12 @@
 @extends('Home.home')
 @section('section_1')
-   <div class="main-content">
-       @foreach ($data as $datum)
-            <div>
-                <h1>{{$datum->airbusname}}</h1>
-            </div>
-       @endforeach
-   </div>
-   {{$data->links()}}
+<div class="container">
+    @foreach ($data as $datum)
+        <div style="border: 15px;">
+            <h1>{{$datum->airbusname}}</h1>
+            <p>{{$datum->airbus_description}}</p>
+        </div>
+    @endforeach
+</div>
+{{$data->links()}}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AirbusController;
 use App\Http\Controllers\ProductHomeController;
-use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 
 /*
 |--------------------------------------------------------------------------
@@ -50,3 +63,19 @@ Route::get('test4/', [ProductHomeController::class, 'TestFour']);
 // Working with Cache 
 Route::get('homePaginatorErr', [ProductHomeController::class, 'HomeWithoutPaginator']);
 Route::get('home', [ProductHomeController::class, 'Home']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
