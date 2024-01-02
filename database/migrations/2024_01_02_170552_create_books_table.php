952132f6f774943485aea3aa707fce574bc47e30
nurulabsr <absar.ru@yandex.com>
2024-01-02 23:19:31 +0600

    added dropSoftDeletes() in the down() function

--- database/migrations/2024_01_02_170552_create_books_table.php
+++ database/migrations/2024_01_02_170552_create_books_table.php
@@ -26,8 +26,10 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void{
+        Schema::table('books', function(Blueprint $table){
+            $table->dropSoftDeletes();
+        });
         Schema::dropIfExists('books');
     }
 };
