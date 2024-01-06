fc75c085ac143744b11a5a50fcf5d15a40656c9d
nurulabsr <absar.ru@yandex.com>
2024-01-06 06:19:59 +0600

    Create post

--- resources/views/Post/Form/create.blade.php
+++ resources/views/Post/Form/create.blade.php
@@ -7,9 +7,13 @@
                     <div class="col-md-6">
                         <h1>Add post</h1>
                     </div>
-                    <div class="col-md-6">
-                        
+                    <div class="col-md-6 d-flex justify-content-end">
+                       <button class="btn btn-success btn-sm">View All Post</button>
+                       <button class="btn btn-warning btn-sm">Recycle Bin</button>
                     </div>
+                </div>
+                <div class="card-body">
+                    <h1>Hello</h1>
                 </div>
             </div>
         </div>    
