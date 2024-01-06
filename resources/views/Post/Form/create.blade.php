37b4545b78548da372f14bed4168f18eda7ba62b
nurulabsr <absar.ru@yandex.com>
2024-01-06 06:10:58 +0600

    form added to create post

---
+++ resources/views/Post/Form/create.blade.php
@@ -0,0 +1,17 @@
+@extends('Home.home')
+@section('section_1')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1>Add post</h1>
+                    </div>
+                    <div class="col-md-6">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div> 
+@endsection
