ede9b8e9e144aca57258e8c3e690eb73c3e8a888
nurulabsr <absar.ru@yandex.com>
2024-01-06 06:04:14 +0600

    PostController added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller{
     
     public function CreatePost(){
-        
+        $posts = Post::all();
+
+        return view('Post.Form.create');
     }
 }
