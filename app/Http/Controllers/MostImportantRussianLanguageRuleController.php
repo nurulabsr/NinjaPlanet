0c8158242eb91637c1176ddc7e0e9c6176b773b6
nurulabsr <absar.ru@yandex.com>
2024-01-09 22:44:03 +0600

    Added russian language most important rule controller, and function in the controller

---
+++ app/Http/Controllers/MostImportantRussianLanguageRuleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MostImportantRussianLanguageRuleController extends Controller{
+    public function AddMostImportantRussianLanguageRule(){
+        
+    }
+}
