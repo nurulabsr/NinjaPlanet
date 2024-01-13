3e4bc8533fea552a7aca4355fe81ce6dbb84a236
nurulabsr <absar.ru@yandex.com>
2024-01-13 12:24:28 +0600

    added detech and attached functions

--- app/Observers/FruitPivotTableObserver.php
+++ app/Observers/FruitPivotTableObserver.php
@@ -1,8 +1,20 @@
 <?php
 
 namespace App\Observers;
-
-class FruitPivotTableObserver
-{
+use Illuminate\Support\Facades\Log;
+class FruitPivotTableObserver{
     //
+
+    public function attached($model, $relationName, $attributes){
+        $fruitId = $model->fruit_id;
+        $tagId = $model->tag_id;
+        Log::info("Fruit ID '{$fruitId}' attached to Tag ID '{$tagId}'", $attributes);
+    }
+
+
+    public function detached($model, $relationName, $attributes){
+        $fruitId = $model->fruit_id;
+        $tagId = $model->tag_id;
+        Log::info("Fruit ID '{$fruitId}' detached from Tag ID '{$tagId}'", $attributes);
+    }
 }
